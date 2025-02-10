888384e9ee8439acb70c31db8a75dcee6afad353
Natnael Eyuel <167537997+natnaeleyuel@users.noreply.github.com>
2025-02-10 16:14:39 +0300

    Time: 792 ms (5.03%), Space: 18.3 MB (41.17%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.py
+++ 2502-sort-the-people/2502-sort-the-people.py
@@ -14,17 +14,23 @@ class Solution:
         #     if isSorted:
         #         break
 
-        for i in range(len(heights)):     # selection sort
-            minInd = i
-            isSwap = False
-            for j in range(i+1, len(heights)):
-                if heights[minInd] > heights[j]:
-                    minInd = j
-                    isSwap = True
-            
-            if isSwap:
-                heights[i], heights[minInd] = heights[minInd], heights[i]
-                
+        # for i in range(len(heights)):     # selection sort
+        #     minInd = i
+        #     isSwap = False
+        #     for j in range(i+1, len(heights)):
+        #         if heights[minInd] > heights[j]:
+        #             minInd = j
+        #             isSwap = True
+
+        #     if isSwap:
+        #         heights[i], heights[minInd] = heights[minInd], heights[i]
+
+
+        for i in range(len(heights)):       # selection sort
+            for j in range(1, len(heights)):
+                if heights[j] < heights[j-1]:
+                    heights[j], heights[j-1] = heights[j-1], heights[j]
+
         heights = heights[::-1]
         namesNew = [""]*len(names)
         for i in range(len(names)):
