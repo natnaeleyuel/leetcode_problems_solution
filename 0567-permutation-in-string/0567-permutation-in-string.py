d5e30007a047d15397f15e73a68ff4385273e25f
Natnael Eyuel <167537997+natnaeleyuel@users.noreply.github.com>
2025-02-13 18:31:23 +0300

    Time: 59 ms (30.91%), Space: 17.8 MB (59.22%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,35 +1,26 @@
-from collections import Counter
-class Solution:
-    def checkInclusion(self, s1: str, s2: str) -> bool:
-        # dictS1 = Counter(s1)
-        # difDict = defaultdict(int)
-        # left = 0
-        # right = len(s1) - 1
-        # for i in range(right+1):
-        #     difDict[s2[i]] += 1
-
-        # while right < len(s2) and len(s1) <= len(s2):
-        #     print(difDict)
-        #     print(dictS1)
-        #     if dictS1 == difDict:
-        #         return True
-        #     difDict[s2[right]] += 1
-        #     difDict[s2[left]] -= 1
-
-        #     right += 1
-        #     left += 1
-        # return False
-        counter1 = Counter(s1)
-        right=len(s1)-1
-        left=0
+class Solution(object):
+    def checkInclusion(self, s1, s2):
+        lenS1, lenS2 = len(s1), len(s2)
+        if lenS1 > lenS2:
+            return False
         
-        while right<len(s2):
-            counter2 = Counter(s2[left:right+1])
-            print(counter2)
-            if counter2== counter1:
+        countS1 = Counter(s1)
+        countS2 = Counter(s2[:lenS1])
+        
+        if countS1 == countS2:
+            return True
+        
+        for i in range(lenS1, lenS2):
+            countS2[s2[i]] += 1
+            countS2[s2[i - lenS1]] -= 1
+            
+            if countS2[s2[i - lenS1]] == 0:
+                del countS2[s2[i - lenS1]]
+            
+            if countS1 == countS2:
                 return True
-            left+=1
-            right+=1
+        
         return False
 
-
+             
+        
