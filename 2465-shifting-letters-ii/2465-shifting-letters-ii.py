dc712c76d0fc878fe1864ecbb1063da9cb24e97d
Natnael Eyuel <167537997+natnaeleyuel@users.noreply.github.com>
2025-02-17 12:11:00 +0300

    Time: 52 ms (86.31%), Space: 41.3 MB (64.91%) - LeetHub

--- 2465-shifting-letters-ii/2465-shifting-letters-ii.py
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.py
@@ -1,29 +1,6 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
-        # arr = [0]*len(s)
-        # for i in range(len(shifts)):
-        #     for j in range(len(s)):
-        #         if j >= shifts[i][0] and j <= shifts[i][1]:
-        #             if shifts[i][2] == 0:
-        #                 arr[j] -= 1
-        #             elif shifts[i][2] == 1:
-        #                 arr[j] += 1
-
-        # new_s = list(s)
-        # for i in range(len(new_s)):
-        #     if arr[i] > 0:
-        #         cur_ord = ord(new_s[i]) - 97 + arr[i]
-        #         cur_ord = cur_ord % 26
-        #         new_s[i] = chr(cur_ord + 97)
-        #     elif arr[i] < 0:
-        #         cur_ord = ord(new_s[i]) - 97 + arr[i]
-        #         if cur_ord > 0:
-        #             new_s[i] = chr(cur_ord + 97)
-        #         else: 
-        #             cur_ord = cur_ord % 26
-        #             new_s[i] = chr(cur_ord + 97)
-        
-
+        # optimized way using prefix sum
         n = len(s)
         diff = [0] * (n + 1)
         for left, right, direction in shifts:
