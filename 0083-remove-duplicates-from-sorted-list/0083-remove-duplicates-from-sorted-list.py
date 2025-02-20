2d156761a081b0ba3fc2350d4cc3f16fd0350146
Natnael Eyuel <167537997+natnaeleyuel@users.noreply.github.com>
2025-02-20 21:42:10 +0300

    Time: 2 ms (48.46%), Space: 12.5 MB (21.11%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def deleteDuplicates(self, head):
+        res = head
+
+        while head and head.next:
+            if head.val == head.next.val:
+                head.next = head.next.next
+            else:
+                head = head.next
+        
+        return res
+        
