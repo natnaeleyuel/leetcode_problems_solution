3506a52fcdc1af85286bcaddcb1e34096b5acc4a
Natnael Eyuel <167537997+natnaeleyuel@users.noreply.github.com>
2025-03-06 13:38:26 +0300

    Time: 2 ms (95.79%), Space: 18.3 MB (45%) - LeetHub

--- 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.py
+++ 3227-find-missing-and-repeated-values/3227-find-missing-and-repeated-values.py
@@ -1,20 +1,20 @@
 class Solution:
     def findMissingAndRepeatedValues(self, grid: List[List[int]]) -> List[int]:
-        numsdict = defaultdict(int)
+        n = len(grid)
+        size = n ** 2
         repeated = 0
         presum = 0
+        freq = [0] * (size + 1)
 
         for row in grid:
-            for col in row:
-                presum += col
-                if numsdict[col] > 0:
-                    repeated = col
-                numsdict[col] += 1
+            for num in row:
+                freq[num] += 1
+                if freq[num] == 2:
+                    repeated = num
+                presum += num
 
-        n = len(grid)
-        n = n ** 2
         presum -= repeated
-        totsum = n * (n + 1) // 2
+        totsum = size * (size + 1) // 2
         missed = totsum - presum
 
         return [repeated, missed]
