f619042e5ee3d77ce5083213ac0a8d849080170e
Natnael Eyuel <167537997+natnaeleyuel@users.noreply.github.com>
2025-03-13 23:32:49 +0300

    Time: 699 ms (59.82%), Space: 63.2 MB (56.88%) - LeetHub

--- 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.py
+++ 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.py
@@ -1,33 +1,29 @@
 class Solution:
     def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
+        def check(nums: List[int], queries: List[List[int]], k: int) -> bool:
+            n = len(nums)
+            tot = 0
+            diff = [0] * (n + 1)
+            for i in range(k):
+                start, end, val = queries[i]
+                diff[start] += val
+                diff[end + 1] -= val
+            for i in range(n):
+                tot += diff[i]
+                if tot < nums[i]:
+                    return False
+            return True
+
         n = len(nums)
-        l, r = 0, len(queries)
-
-        if not self.check(nums, queries, r):
+        l = 0
+        r = len(queries)
+        if not check(nums, queries, r):
             return -1
-
         while l <= r:
             m = l + (r - l) // 2
-            if self.check(nums, queries, m):
+            if check(nums, queries, m):
                 r = m - 1
             else:
                 l = m + 1
-
         return l
 
-    def check(self, nums: List[int], queries: List[List[int]], k: int) -> bool:
-        n = len(nums)
-        tot = 0
-        diff = [0] * (n + 1)
-
-        for i in range(k):
-            start, end, val = queries[i]
-
-            diff[start] += val
-            diff[end + 1] -= val
-
-        for i in range(n):
-            tot += diff[i]
-            if tot < nums[i]:
-                return False
-        return True
