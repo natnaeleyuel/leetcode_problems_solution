e998fd36c5b5cc034fed72b29207ce7dccf66f90
Natnael Eyuel <167537997+natnaeleyuel@users.noreply.github.com>
2025-03-17 18:39:36 +0300

    Time: 1 ms (12.34%), Space: 19.4 MB (43.38%) - LeetHub

--- 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.py
+++ 0783-search-in-a-binary-search-tree/0783-search-in-a-binary-search-tree.py
@@ -1,18 +1,12 @@
 
 class Solution:
     def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
-        if not root:
-            return None
-        deq = deque([root])
-        node = None
-        while deq:
-            for i in range(len(deq)):
-                poped = deq.popleft()
-                if poped.val == val:
-                    node = poped
-                if poped.left:
-                    deq.append(poped.left)
-                if poped.right:
-                    deq.append(poped.right)
-       
-        return node
+        node = root
+        while node:
+            if node.val > val:
+                node = node.left
+            elif node.val < val:
+                node = node.right
+            else:
+                return node
+        return None
