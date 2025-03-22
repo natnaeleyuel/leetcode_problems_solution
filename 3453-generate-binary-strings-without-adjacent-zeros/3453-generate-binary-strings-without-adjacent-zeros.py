b2e04796b3654e18f6a6abdc3a38c71930d43dc7
Natnael Eyuel <167537997+natnaeleyuel@users.noreply.github.com>
2025-03-22 22:33:24 +0300

    Time: 55 ms (66.98%), Space: 19 MB (75.29%) - LeetHub

--- 3453-generate-binary-strings-without-adjacent-zeros/3453-generate-binary-strings-without-adjacent-zeros.py
+++ 3453-generate-binary-strings-without-adjacent-zeros/3453-generate-binary-strings-without-adjacent-zeros.py
@@ -1,14 +1,14 @@
 class Solution:
     def validStrings(self, n: int) -> List[str]:
-        output = ["0","1"]
+        result = ["0","1"]
         for i in range(n-1):
-            tmp = []
-            for bin_str in output:
+            curr = []
+            for bin_str in result:
                 if bin_str[-1] == '0':
-                    tmp.append(bin_str+'1')
+                    curr.append(bin_str + '1')
                 else:
-                    tmp.append(bin_str+'0')
-                    tmp.append(bin_str+'1')
-            output = tmp
+                    curr.append(bin_str + '0')
+                    curr.append(bin_str + '1')
+            result = curr
 
-        return output
+        return result
