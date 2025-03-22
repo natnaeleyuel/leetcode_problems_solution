4678a94a6b0a824546631fe58da80f95d2bdff1b
Natnael Eyuel <167537997+natnaeleyuel@users.noreply.github.com>
2025-03-22 22:14:56 +0300

    Time: 768 ms (60.75%), Space: 17.7 MB (71.37%) - LeetHub

--- 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.py
+++ 2802-find-the-punishment-number-of-an-integer/2802-find-the-punishment-number-of-an-integer.py
@@ -1,6 +1,6 @@
 class Solution:
     def punishmentNumber(self, n: int) -> int:
-        def is_square(s: str, i: int, x: int) -> bool:
+        def recur(s, i, x):
             m = len(s)
             if i >= m:
                 return x == 0
@@ -9,17 +9,17 @@ class Solution:
                 y = y * 10 + int(s[j])
                 if y > x:
                     break
-                if is_square(s, j + 1, x - y):
+                if recur(s, j + 1, x - y):
                     return True
             return False
 
-        punishment = 0
+        pun = 0
         for i in range(1, n+1):
             x = i * i
-            if is_square(str(x), 0, i):
-                punishment += x
+            if recur(str(x), 0, i):
+                pun += x
         
-        return punishment
+        return pun
             
 
         
