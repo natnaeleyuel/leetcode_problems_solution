2d0358e0f45d45f239bddf850a8966158e7f0ba5
Natnael Eyuel <167537997+natnaeleyuel@users.noreply.github.com>
2025-03-30 16:52:42 +0300

    Time: 7 ms (41.22%), Space: 17.9 MB (36.45%) - LeetHub

--- 0768-partition-labels/0768-partition-labels.py
+++ 0768-partition-labels/0768-partition-labels.py
@@ -1,14 +1,14 @@
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        counterChr = defaultdict(int)
+        endind = defaultdict(int)
         for i in range(len(s) - 1, -1, -1):
-            if s[i] not in counterChr:
-                counterChr[s[i]] = i
+            if s[i] not in endind:
+                endind[s[i]] = i
             
         result = []
-        maxInd = counterChr[s[0]]
+        maxInd = endind[s[0]]
         for i in range(len(s)):
-            maxInd = max(maxInd, counterChr[s[i]])
+            maxInd = max(maxInd, endind[s[i]])
             if maxInd == i:
                 result.append(i + 1 - sum(result))
             
