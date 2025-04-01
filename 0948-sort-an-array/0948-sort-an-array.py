e40828253cd8e5774063e631b33e89efa6efb73d
Natnael Eyuel <167537997+natnaeleyuel@users.noreply.github.com>
2025-04-01 18:58:14 +0300

    Time: 817 ms (15.54%), Space: 24.4 MB (70.7%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.py
+++ 0948-sort-an-array/0948-sort-an-array.py
@@ -1,49 +1,48 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        n = len(nums)
+        def merge(arr, left, mid, right):
+            n1 = mid - left + 1
+            n2 = right - mid
 
-        def merge(nums, l, m, r):
-            num1 = m + 1 - l
-            num2 = r - m
+            leftarr = []
+            rightarr = []
 
-            listLeft = [0] * num1
-            listRight = [0] * num2
-
-            for i in range(num1):
-                listLeft[i] = nums[l + i]
-            for j in range(num2):
-                listRight[j] = nums[m + j + 1]
+            for i in range(left, mid + 1):
+                leftarr.append(arr[i])
             
-            i = 0
-            j = 0
-            k = l
+            for j in range(mid + 1, right + 1):
+                rightarr.append(arr[j])
 
-            while i < num1 and j < num2:
-                if listLeft[i] <= listRight[j]:
-                    nums[k] = listLeft[i]
+            i, j, k = 0, 0, left
+
+            while i < n1 and j < n2:
+                if leftarr[i] <= rightarr[j]:
+                    arr[k] = leftarr[i]
                     i += 1
                 else:
-                    nums[k] =  listRight[j]
+                    arr[k] = rightarr[j]
                     j += 1
                 k += 1
             
-            while i < num1:
-                nums[k] = listLeft[i]
+            while i < n1:
+                arr[k] = leftarr[i]
                 i += 1
                 k += 1
-             
-            while j < num2:
-                nums[k] = listRight[j]
+            
+            while j < n2:
+                arr[k] = rightarr[j]
                 j += 1
                 k += 1
-
-        def mergeSort(nums, l, r):
-            if l < r:
-                m = l + (r-l) // 2
-
-                mergeSort(nums, l, m)
-                mergeSort(nums, m + 1, r)
-                merge(nums, l, m, r)
         
-        mergeSort(nums, 0, n-1)
+        def mergesort(arr, left, right):
+            if left >= right:
+                return []
+            mid = (left + right) // 2
+
+            mergesort(arr, left, mid)
+            mergesort(arr, mid + 1, right)
+            merge(arr, left, mid, right)
+        
+
+        mergesort(nums, 0, len(nums) - 1)
         return nums
