b8c432f79e2497d5a7239b4429171bbb45b4fdd9
Natnael Eyuel <167537997+natnaeleyuel@users.noreply.github.com>
2025-04-09 01:45:01 +0300

    Time: 0 ms (100%), Space: 17.8 MB (74.14%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.py
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.py
@@ -1,10 +1,8 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        distinct = set()
-        n = len(nums)
-        for i in range(n - 1, - 1, - 1):
-            if nums[i] not in distinct:
-                distinct.add(nums[i])
-            else:
+        seen = set()
+        for i in range(len(nums) - 1, -1, -1):
+            if nums[i] in seen:
                 return math.ceil((i + 1) / 3)
+            seen.add(nums[i])
         return 0
