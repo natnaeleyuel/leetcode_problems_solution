2402d413e64a785f25a58ee5bd1698d377a71062
Natnael Eyuel <167537997+natnaeleyuel@users.noreply.github.com>
2025-05-02 18:09:26 +0300

    Time: 10 ms (30.86%), Space: 19 MB (68.97%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,22 +1,44 @@
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        # connected = defaultdict(list)
+        # n = len(isConnected)
+        # for i in range(n):
+        #     for j in range(n):
+        #         if i != j and isConnected[i][j] == 1:
+        #             connected[i + 1].append(j + 1)
+        
+        # visited = set()
+        # def dfs(node):
+        #     visited.add(node)
+        #     for neigh in connected[node]:
+        #         if neigh not in visited:
+        #             dfs(neigh)
+        #     return 1
+        
+        # res = 0
+        # for node in range(1, n + 1):
+        #     if node not in visited:
+        #         res += dfs(node)
+        # return res
+
         connected = defaultdict(list)
         n = len(isConnected)
         for i in range(n):
             for j in range(n):
                 if i != j and isConnected[i][j] == 1:
                     connected[i + 1].append(j + 1)
-        
-        visited = set()
-        def dfs(node):
-            visited.add(node)
-            for neigh in connected[node]:
-                if neigh not in visited:
-                    dfs(neigh)
+        root = defaultdict(int)
+        def solve(node):
+            root[node] = 1
+            for nei in connected[node]:
+                if root[nei] == 0:
+                    solve(nei)
             return 1
-        
-        res = 0
+
+        provinces = 0
         for node in range(1, n + 1):
-            if node not in visited:
-                res += dfs(node)
-        return res
+            if root[node] == 0:
+                provinces += solve(node)
+        
+        return provinces
+        
