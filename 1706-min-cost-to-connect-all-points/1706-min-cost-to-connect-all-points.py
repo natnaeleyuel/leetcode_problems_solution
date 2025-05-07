077263711bda310b526d43f5a904cede4fe9e176
Natnael Eyuel <167537997+natnaeleyuel@users.noreply.github.com>
2025-05-08 00:02:02 +0300

    Time: 635 ms (82.34%), Space: 85.4 MB (55.31%) - LeetHub

---
+++ 1706-min-cost-to-connect-all-points/1706-min-cost-to-connect-all-points.py
@@ -0,0 +1,46 @@
+def md(a: List[int], b: List[int]) -> int:
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+class UF:
+    def __init__(self, n):
+        self.p = [i for i in range(n)]
+        self.r = [0] * n
+        
+    def f(self, u):
+        if self.p[u] != u:
+            self.p[u] = self.f(self.p[u])
+        return self.p[u]
+    
+    def u(self, u, v):
+        u, v = self.f(u), self.f(v)
+        if u == v:
+            return False
+        if self.r[u] > self.r[v]:
+            u, v = v, u
+        self.p[u] = v
+        if self.r[u] == self.r[v]:
+            self.r[v] += 1
+        return True
+
+class Solution:
+    def minCostConnectPoints(self, pts: List[List[int]]) -> int:
+        n = len(pts)
+        dsu = UF(n)
+        heap = []
+        
+        for i in range(n):
+            for j in range(i+1, n):
+                heappush(heap, (md(pts[i], pts[j]), i, j))
+        
+        res = 0
+        cnt = 0
+        
+        while heap:
+            w, u, v = heappop(heap)
+            if dsu.u(u, v):
+                res += w
+                cnt += 1
+                if cnt == n - 1:
+                    break
+                    
+        return res
