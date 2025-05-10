9d7c058d0c5f07e3f11992c505e4611694b19c4a
Natnael Eyuel <167537997+natnaeleyuel@users.noreply.github.com>
2025-05-10 23:13:33 +0300

    Time: 2480 ms (5.34%), Space: 118.9 MB (5.35%) - LeetHub

---
+++ 1507-check-if-there-is-a-valid-path-in-a-grid/1507-check-if-there-is-a-valid-path-in-a-grid.py
@@ -0,0 +1,44 @@
+class DisjointUnion:
+    def __init__(self, rows, cols):
+        self.root = {}
+        for i in range(-1, rows * 2):
+            for j in range(-1, cols * 2):
+                self.root[(i, j)] = (i, j)
+    
+    def find(self, x):
+        if self.root[x] != x:
+            self.root[x] = self.find(self.root[x])
+        return self.root[x]
+    
+    def merge(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        if root_x != root_y:
+            self.root[root_x] = root_y
+
+class Solution:
+    def hasValidPath(self, grid):
+        if not grid or not grid[0]:
+            return False
+        
+        m, n = len(grid), len(grid[0])
+        dsu = DisjointUnion(m, n)
+        
+        connection_rules = {
+            1: [(0, -1), (0, 1)],   
+            2: [(-1, 0), (1, 0)],  
+            3: [(0, -1), (1, 0)],   
+            4: [(0, 1), (1, 0)],  
+            5: [(-1, 0), (0, -1)], 
+            6: [(-1, 0), (0, 1)]   
+        }
+        
+        for i in range(m):
+            for j in range(n):
+                cell_type = grid[i][j]
+                for di, dj in connection_rules[cell_type]:
+                    dsu.merge((i * 2, j * 2), (i * 2 + di, j * 2 + dj))
+        
+        start = (0, 0)
+        end = (m * 2 - 2, n * 2 - 2)
+        return dsu.find(start) == dsu.find(end)
