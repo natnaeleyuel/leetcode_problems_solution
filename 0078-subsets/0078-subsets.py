11cafec8316bc8bbcb14897fd41f9e8d20158b24
Natnael Eyuel <167537997+natnaeleyuel@users.noreply.github.com>
2025-05-21 01:13:14 +0300

    Time: 0 ms (100%), Space: 17.8 MB (65.15%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,22 +1,5 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        # 1, Backtracking solution 
-        # def subsets_create(k):
-        #     if k == len(nums):
-        #         result.append(new_arr[:])
-        #         return
-        #     else:
-        #         new_arr.append(nums[k])
-        #         subsets_create(k+1)
-        #         new_arr.pop()
-        #         subsets_create(k+1)
-
-        # result = []
-        # new_arr = []
-        # subsets_create(0)
-        # return result
-
-        # 2, Biwise solution 
         n = len(nums)
         tot = 1 << n
         result = []
