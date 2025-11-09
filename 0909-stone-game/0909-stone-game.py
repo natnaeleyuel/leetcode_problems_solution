1c15693a3eb643b2e55ce1b617617b4c0c4f6800
Natnael Eyuel <167537997+natnaeleyuel@users.noreply.github.com>
2025-11-09 23:52:09 +0300

    Time: 519 ms (10.5%), Space: 129.1 MB (10.5%) - LeetHub

--- 0909-stone-game/0909-stone-game.py
+++ 0909-stone-game/0909-stone-game.py
@@ -1,3 +1,23 @@
 class Solution:
     def stoneGame(self, piles: List[int]) -> bool:
-        return True
+        n = len(piles)
+        dp = {}
+
+        def dfs(l, r):
+            if l > r:
+                return 0
+            if (l, r) in dp:
+                return dp[(l, r)]
+
+            check = True if (r - l) % 2 else False
+            lft = piles[l] if check else 0
+            rgt = piles[r] if check else 0
+
+            dp[(l, r)] = max(
+                dfs(l + 1, r) + lft,
+                dfs(l, r - 1) + rgt
+            )
+
+            return dp[(l, r)]
+        
+        return dfs(0, n - 1) > sum(piles) // 2
